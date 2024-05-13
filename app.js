672455036c23c7735efaecdb35c344acd663ccfa
jthapata <jessezap01@gmail.com>
2024-05-13 09:08:53 -0600

    adding code

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.get('/students?/:id', (req,res)=>{
+    res.send(`Request: ${req.params.id}\n`)
+})
+app.post('/newStudent', (req, res)=>{
+    const firstName = req.body.fName
+    res.send(`${firstName}`)
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
